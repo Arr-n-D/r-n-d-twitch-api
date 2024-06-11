a060061f67543a4411429662588026191dcbe1fb
Renaud <renaudgdumont@gmail.com>
2024-06-11 13:12:02 -0400

    first commit

---
+++ database/migrations/0001_01_01_000000_create_members_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('twitch_user_id')->unique();
+            $table->string('twitch_username')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
